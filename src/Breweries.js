3dbeeaec3ce96d289dc445fa7e5486050d9f576c
Nico Paulino <nicopaulino8@gmail.com>
2022-07-06 19:16:42 -0500

    Add Bootstrap Accordian

--- src/Breweries.js
+++ src/Breweries.js
@@ -1,40 +1,92 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './App.css';
 import axios from 'axios';
-import {GoogleMap, useLoadScript, Marker} from '@react-google-maps/api'
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+
+const containerStyle = {
+  width: '18rem',
+  height: '18rem'
+};
+
+
 
 
 function Breweries() {
 
   const [barsList, setBarsList] = useState([]);
+  const [map, setMap] = useState(null)
+  const [center, setCenter] = useState({
+    lat: 29.9557185,
+    lng: -90.0639242
+  })
 
 
-  const generateBars = () => {
- 
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_API_KEY
+  })
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+
+  useEffect(() => {
     axios.get('https://api.openbrewerydb.org/breweries?by_city=new_orleans')
         .then(response => setBarsList(response.data))
         .then(console.log(barsList));
+  }, []);
 
-      };
+  // const generateBars = () => {
+  //   axios.get('https://api.openbrewerydb.org/breweries?by_city=new_orleans')
+  //       .then(response => setBarsList(response.data))
+  //       .then(console.log(barsList));
+  //     };
 
   return (
   
     <div className="App">
       <header className="App-header">
-        <div>Hello World</div>
-        <button onClick={() => generateBars()}>Get Bars</button>
-            <div>
-                {barsList.map(bar => {
+        <h1>Breweries in New Orleans</h1>
+        {/* <button onClick={() => generateBars()}>Get Bars</button> */}
+        <Accordion style={{ width: '90%', color: 'black' }}>
+                {barsList.map((bar, i) => {
                     return (
-                        <div>
-                            <div>Bar: {bar.name}</div>
-                            <div>Type: {bar.brewery_type}</div>
-                            <div>Address: {bar.street} </div>
-                            <a target="_blank" href={`${bar.website_url}`}>Website</a>
-                        </div>
+                            <Accordion.Item eventKey={i} key={i}>
+                            <Accordion.Header onClick={() => setCenter({lat: parseFloat(bar.latitude), lng: parseFloat(bar.longitude)})}>{bar.name}</Accordion.Header>
+                            <Accordion.Body>
+                                <div>Brewery Type: {bar.brewery_type}</div>
+                                <div>Address: {bar.street}</div>
+                                <div>{`${bar.city}, ${bar.state} ${bar.postal_code}`}</div>
+                                {bar.website_url ? <a target="_blank" href={`${bar.website_url}`}>Website</a> : null}
+                                { isLoaded && bar.latitude ? (
+                                    <GoogleMap
+                                      mapContainerStyle={containerStyle}
+                                      center={center}
+                                      // zoom={20}
+                                      onLoad={onLoad}
+                                      onUnmount={onUnmount}
+                                    >
+                                      <Marker position={center}/>
+                                      { /* Child components, such as markers, info windows, etc. */ }
+                                      <></>
+                                    </GoogleMap>
+                                ) : null }
+                            </Accordion.Body>
+                            </Accordion.Item>
+
                     )
                 })}
-            </div>
+            </Accordion>
         
       </header>
     </div>

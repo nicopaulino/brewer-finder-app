c6f334e6c519232ceba503fc635d844a935176dd
Nico Paulino <nicopaulino8@gmail.com>
2022-07-06 19:16:22 -0500

    Remove Router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import {GoogleMap, useLoadScript, Marker} from '@react-google-maps/api'
-import Map from './Map';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Map from './OldMap';
 import Breweries from './Breweries';
+// import Breweries from './Brews';
+import Brewery from './Brewery';
 
 
 function App() {
@@ -11,16 +14,21 @@ function App() {
   const [flag, setFlag] = useState(false);
 
   return (
-  
     <div className="App">
       <header className="App-header">
-        {flag ? 
-        <Map/> : 
-        <Breweries/>
-        }
+      <Breweries/>
       </header>
     </div>
-  );
-}
+  //   <BrowserRouter>
+  //     {/* <header>
+  //       <Header></Header>
+  //     </header> */}
+  //     <Routes>
+  //       <Route path="/" element={<Breweries/>} />
+  //       <Route path="/brewery" element={<Brewery/>} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
+)}
 
 export default App;
